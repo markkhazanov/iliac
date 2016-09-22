9faa7988ab1d51d62b180271233b63ecc0eef4a0
Mark Khazanov <markkhazanov@gmail.com>
2016-09-22 07:57:58 -0700

    landing page content added

---
+++ src/components/firebase.js
@@ -0,0 +1,14 @@
+    <script src="https://www.gstatic.com/firebasejs/3.4.0/firebase.js"></script>
+
+      // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCd8UY6w9_pKeCHqT9iFt7RDQJvm58lixI",
+  authDomain: "iliac-57f28.firebaseapp.com",
+  databaseURL: "https://iliac-57f28.firebaseio.com",
+  storageBucket: "iliac-57f28.appspot.com",
+  messagingSenderId: "321314430827"
+};
+firebase.initializeApp(config);
+
+
+//http://stackoverflow.com/questions/37337080/initialize-firebase-references-in-two-separate-files-in-the-new-api

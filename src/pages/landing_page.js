bdcd9d61e00be24a048da2d6d6b0820efcc0dd81
Mark Khazanov <markkhazanov@gmail.com>
2016-09-24 15:51:57 -0700

    everything without icons

--- src/pages/landing_page.js
+++ src/pages/landing_page.js
@@ -5,18 +5,24 @@ import Login from '../components/login';
 
 class LandingPage extends Component {
 	componentWillMount() {
-		const script = document.createElement("script");
+		const scrollReveal = document.createElement("scrollReveal");
+		const parallax = document.createElement("parallax");
 
-    script.src = "https://cdn.jsdelivr.net/scrollreveal.js/3.2.0/scrollreveal.min.js";
-    script.async = true;
+    scrollReveal.src = "https://cdn.jsdelivr.net/scrollreveal.js/3.2.0/scrollreveal.min.js";
+    scrollReveal.async = true;
+    parallax.src = "../../../style/js/parallax.js"
+    parallax.async = true;
 
-    document.body.appendChild(script);
+    document.body.appendChild(scrollReveal);
+    document.body.appendChild(parallax);
 
 	}
 
 	componentDidMount(){
 		window.sr = ScrollReveal();
-    sr.reveal('p', {duration: 3000});
+    sr.reveal('p', {duration: 2000});
+
+    $('#mockups').parallax({imageSrc: '../../../style/img/info.jpg'});
 	}
 
 	render(){
@@ -36,9 +42,42 @@ class LandingPage extends Component {
 				</div>
 
 				<div id="mockups" className="row">
-					<img src="../../../style/img/3-screens.png"/>
-					<p> We break down barriers and create greater 
-					efficiencies within the industry.</p>
+					<h2>How Iliac transforms your flow</h2>
+
+					<div id="features"> 
+						<div id="box1" className="feature col-md-4">
+							<img src="../../../style/img/project.png"/>
+							<h3>Projects</h3>
+							<p>
+								Creating your own content? You can use our 
+								projects feature to book talent directly, 
+								publicize open positions, and sync the 
+								details of a shoot to everyone’s account.
+							</p>
+						</div>
+
+						<div id="box2" className="feature col-md-4">
+							<img src="../../../style/img/jobs-feed.png"/>
+							<h3>Jobs Feed</h3>
+							<p>
+								When there’s work available we’ll see if 
+								you match up based on your yes/no list and 
+								personal characteristics. The posting will 
+								appear on your job feed and so you’ll always 
+								have a shoot waiting for you.
+							</p>
+						</div>
+						
+						<div id="box3" className="feature col-md-4">
+							<img src="../../../style/img/paypal.png"/>
+							<h3>Payments</h3>
+							<p>
+								We work with PayPal so you can transfer payments 
+								and focus on the more important things.
+							</p>
+						</div>
+					</div>
+
 				</div>
 
 
@@ -48,44 +87,16 @@ class LandingPage extends Component {
 						training tools to help you decide whether entering 
 						is right for you. Our education suite was created 
 						with the help of APAC, the FSC, and Kink.com so you 
-						can be sure the information your getting is accurate 
-						and informative.
+						can be sure the information you're getting is accurate 
+						and informative.'
 					</p>
 				</div>
-
-				<div id="features" className="row"> 
-					<div id="box1" className="feature col-md-4">
-						<p>
-							Creating your own content? You can use our 
-							projects feature to book talent directly, 
-							publicize open positions, and sync the 
-							details of a shoot to everyone’s account.
-						</p>
-					</div>
-
-					<div id="box2" className="feature col-md-4">
-						<p>
-							When there’s work available we’ll see if 
-							you match up based on your yes/no list and 
-							personal characteristics. The posting will 
-							appear on your job feed and so you’ll always 
-							have a shoot waiting for you.
-						</p>
-					</div>
-					
-					<div id="box3" className="feature col-md-4">
-						<p>
-							We work with PayPal so you can transfer payments 
-							and focus on the more important things.
-						</p>
-					</div>
-				</div>
-
 
 
 				<div id="safety" className="row">
 					<h2>How do we keep you safe?</h2>
 					<div className="col-md-3">
+						<h3>Information</h3>
 						<p>
 							We will never force you to input information
 							 that you are uncomfortable sharing. Even your 
@@ -95,6 +106,7 @@ class LandingPage extends Component {
 					</div>
 					
 					<div className="col-md-3">
+						<h3>Reporting</h3>
 						<p>
 							If there’s ever a problem on or off set you 
 							can file an anonymous claim that will be forwarded 
@@ -105,6 +117,7 @@ class LandingPage extends Component {
 					</div>
 					
 					<div className="col-md-3">
+						<h3>Transparency</h3>
 						<p>
 							We provide you with studio terms and conditions 
 							before you step on set. Greater transparency means 
@@ -114,6 +127,7 @@ class LandingPage extends Component {
 					</div>
 					
 					<div className="col-md-3">
+						<h3>Resources</h3>
 						<p>
 							Our resources information will guide you in 
 							finding doctors, lawyers, and even financial 
@@ -123,7 +137,12 @@ class LandingPage extends Component {
 					</div>
 				</div>
 
-				<Login />
+				<div id="beta" className="row">
+
+					<div id="login"><Login /></div>
+
+				</div>
+
 				<hr/>
 				<div id="l-footer">Footer</div>
 			</div>

be9bcd56033816a5616908d9ac9b98d5d2066666
Mark Khazanov <markkhazanov@gmail.com>
2016-10-03 14:00:38 -0700

    change index path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './src/index'
+    path.join(__dirname, 'src', 'index')
   ],
   output: {
     path: path.join(__dirname, 'dist'),
